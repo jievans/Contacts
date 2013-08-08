2ebae0a2bca2bfeaa88178e8d8faf184b5ddbec4
Josh Evans <vitocairone@gmail.com>
2013-08-07 23:46:04 -0700

    Change to app controller

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,30 +1,6 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery
 
-  before_filter :verify_logged
-  before_filter :authorize_user
-
-
-  def authorize_user
-    puts authorize_helper
-    authorize_helper
-  end
-
-  def authorize_helper
-    return false unless logged_in?
-    if params.keys.include?("user_id")
-      current_user.id == params[:user_id]
-    elsif params.keys.include?(self_class_sym) &&
-    params[self_class_sym].keys.include?("user_id")
-      current_user.id == params[self_class_sym]["user_id"]
-    elsif params.keys.include?("id")
-      current_user.id == params["id"]
-    else
-      false
-    end
-  end
-
-
   def current_user
     @current_user ||= User.find_by_token(params[:token])
   end
